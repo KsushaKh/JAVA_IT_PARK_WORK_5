31bb3ac5cb0693daa468a99bd5d481b541c3f2ed
Student11 <student11@itpark.ru>
2018-03-22 19:30:09 +0300

    add classworks with task1

---
+++ ClassWorks/Task 1/Program.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Program {
+	public static void main(String args[]){
+		Scanner scanner = new Scanner(System.in);
+		
+		int numder = scanner.nextInt();
+		
+		System.out.println(numder);
+	}
+	
+}
